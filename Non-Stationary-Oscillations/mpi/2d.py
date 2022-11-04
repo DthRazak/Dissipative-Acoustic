0f5fd10a32dd9a589c0507b79cd0125e7ab104e4
Volodymyr Milchanovskyi <vovamilchan@gmail.com>
2022-11-04 19:10:39 +0000

    Added mpi scripts with grid search for Non-Stationary problem

--- Non-Stationary-Oscillations/mpi/2d.py
+++ Non-Stationary-Oscillations/mpi/2d.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from dolfinx import fem, mesh, io
+from urllib.request import ftpwrapper
+from dolfinx import geometry, fem, mesh, plot, io
 from mpi4py import MPI
 from petsc4py import PETSc
-from petsc4py.PETSc import ScalarType
-from ufl import (TrialFunction, TestFunction, dx, grad, div, inner, lhs, rhs)
+from petsc4py.PETSc import ScalarType, ComplexType
+from ufl import (TrialFunction, Measure, TestFunction, dx, ds, grad, div, inner, lhs, rhs)
 
 from dolfinx.common import Timer, TimingType, list_timings
 
@@ -14,54 +15,55 @@ import pandas as pd
 
 from utils.dolfinx import (BoundaryCondition, generate_boundary_measure, 
                            project, L2_norm, H1_norm)
+from utils.meshing import create_square_mesh
 
 from pathlib import Path
 
 
-save_dir = "./results/1dh-non-stationary"
+save_dir = "./results/2dh"
 Path(save_dir).mkdir(parents=True, exist_ok=True)
 
 
-def generate_boundaries(points):
-    return [(1, lambda x: np.isclose(x[0], points[0])), 
-            (2, lambda x: np.isclose(x[0], points[1]))]
-
-def problem_setup(N, points, p, T, t, ct, dt, fluid, solver, pc, prefix):
+def problem_setup(N, points, p, T, t, ct, dt, fluid, solver, pc, prefix):    
     """
     Performs problem configuration w.r.t. given parameters
     """
     
     # Mesh and function space definition
-    domain = mesh.create_interval(MPI.COMM_WORLD, N, points)
-    V = fem.FunctionSpace(domain, ("CG", 1))
+    domain, mt, ft = create_square_mesh(points[0][1], N, MPI.COMM_WORLD, 0)
+
+    V = fem.VectorFunctionSpace(domain, ("CG", 2))
 
     u = TrialFunction(V)
     v = TestFunction(V)
     
-    # Definition of density and speed functions
+    # Definition functions of physical characteristics
     fluids = pd.read_csv('../../data/physical_properties.csv', sep=';', index_col='Fluid')
     
     ro, c, eta = fluids.loc[fluid, ['Density', 'Speed of sound', 'Viscosity']]
         
-    # Construction of bilinear form and linear functional
+    # Construction of problem form
     GAMMA, BETA = 0.5, 0.5
     
     mm = ScalarType(ro) * inner(u, v) * dx
     aa = ScalarType(ro * c**2) * inner(grad(u), grad(v)) * dx
     cc = ScalarType(4./3 * eta) * inner(grad(u), grad(v)) * dx
     
-    F = mm + (dt * GAMMA * cc) + (0.5 * dt * dt * BETA * aa)
-        
-    boundaries = generate_boundaries(points)
-    measure = generate_boundary_measure(boundaries, domain)
+    F = mm + (dt * GAMMA * cc) + (0.5 * dt**2 * BETA * aa)
     
-    u_D = lambda x: x[0] * 0.0
-    u_N1 = fem.Constant(domain, ScalarType(-p))
-    u_N2 = fem.Constant(domain, ScalarType(0.0))
+    # Definition of boundary conditions
+    measure = generate_boundary_measure([], domain, ft)
     
-    bcs = [BoundaryCondition("Dirichlet", 1, u_D, V, u, v, measure).bc]
-    nbcs = [BoundaryCondition("Neumann", 2, u_N1, V, u, v, measure).bc,
-            BoundaryCondition("Neumann", 2, u_N2, V, u, v, measure).bc] 
+    u_D = lambda x: [x[0] * 0.0, x[1] * 0.0, x[2] * 0.0]
+    u_N1 = fem.Constant(domain, ScalarType(p))
+    u_N2 = fem.Constant(domain, ScalarType((0, 0, 0)))
+
+    bcs = [BoundaryCondition("Dirichlet", 1, u_D, V, u, v, measure).bc,
+           BoundaryCondition("Dirichlet", 2, u_D, V, u, v, measure).bc, 
+           BoundaryCondition("Dirichlet", 3, u_D, V, u, v, measure).bc]
+     
+    nbcs = [BoundaryCondition("Neumann", 4, u_N1, V, u, v, measure).bc,
+            BoundaryCondition("Neumann", 4, u_N2, V, u, v, measure).bc] 
     
     return {
         'Params': (N, points, fluid, ro, c, T, dt, t, ct, GAMMA, BETA, solver, pc, prefix),
@@ -70,31 +72,30 @@ def problem_setup(N, points, p, T, t, ct, dt, fluid, solver, pc, prefix):
         'Problem': (F, bcs, nbcs)
     }
 
-
 def solve_problem(config):
     N, points, fluid, ro, c, T, dt, t_stop, ct, GAMMA, BETA, solver_type, pc, prefix = config['Params']
     domain, V, u, v = config['FunctionSpace']
     mm, aa, cc = config['Forms']
     F, bcs, nbcs = config['Problem']
-    
+        
     if prefix:
         PREFIX = f'{solver_type}_{pc}_{MPI.COMM_WORLD.size}_'
     else:
         PREFIX = ''
-
+    
     U_PATH = f"{save_dir}/{PREFIX}uj.xdmf"
     UD_PATH = f"{save_dir}/{PREFIX}uj_d.xdmf"
     PRESSURE_PATH = f"{save_dir}/{PREFIX}pressure.xdmf"
 
     xdmf_u = io.XDMFFile(domain.comm, U_PATH, "w")
-    xdmf_u_d = io.XDMFFile(domain.comm, UD_PATH, "w")
+    xdmf_ud = io.XDMFFile(domain.comm, UD_PATH, "w")
     xdmf_p = io.XDMFFile(domain.comm, PRESSURE_PATH, "w")
     xdmf_u.write_mesh(domain)
-    xdmf_u_d.write_mesh(domain)
+    xdmf_ud.write_mesh(domain)
     xdmf_p.write_mesh(domain)
-
+    
     # Create initial condition
-    initial_condition = lambda x: x[0] * 0.0
+    initial_condition = lambda x: [x[0] * 0.0, x[1] * 0.0, x[2] * 0.0]
     
     uj = fem.Function(V)
     uj_d = fem.Function(V)
@@ -106,10 +107,9 @@ def solve_problem(config):
     uj_dd.interpolate(initial_condition)
     uj_d_d_tg.interpolate(initial_condition)
     
-    p = project(ro * c**2 * uj.dx(0), domain, ('CG', 1))
-    p.x.array[:] = ro * c**2 * uj_d.x.array
+    p = project(ro * c**2 * div(uj), domain, ("CG", 1))
     
-    # Construct the left and right hand side of the problem
+    # Construct the right hand side of the problem
     bilinear_form = fem.form(lhs(F))
     
     A = fem.petsc.assemble_matrix(bilinear_form, bcs=bcs)
@@ -119,7 +119,7 @@ def solve_problem(config):
     solver.setOperators(A)
     solver.setType(solver_type.lower())
     solver.getPC().setType(pc.lower())
-    
+
     uj_d_d_tg.x.array[:] = dt * GAMMA * uj_d.x.array
         
     L1 = nbcs[0] + (cc * uj_d) + (aa * uj) + (aa * uj_d_d_tg)
@@ -133,20 +133,18 @@ def solve_problem(config):
     has_norm = False
     L2_n, H1_n = 0.0, 0.0
 
-    # Solve problem at each time step,
+    # Solve problem at each time step
     num_steps = int(T / dt)
     save_step = 1 if num_steps // 100 < 1 else num_steps // 100
     t = 0.0
-
-    xdmf_u.write_function(uj, t)
-    xdmf_u_d.write_function(uj_d, t)
-    xdmf_p.write_function(p, t)
-
+    
     for i in range(num_steps):
         t += dt
-        
-        uj_d_d_tg.x.array[:] = dt * GAMMA * uj_d.x.array
-        
+
+        # Multiply values of uj_d by dt and GAMMA, 
+        # because form does not support muliplication by constant
+        uj_d_d_tg.x.array[:] = uj_d.x.array * dt * GAMMA
+
         b = b1 if t < t_stop else b2
         linear_form = linear_form_1 if t < t_stop else linear_form_2
 
@@ -158,39 +156,41 @@ def solve_problem(config):
         fem.petsc.apply_lifting(b, [bilinear_form], [bcs])
         b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
         fem.petsc.set_bc(b, bcs)
-        
+
         # Solve linear problem
         solver.solve(b, uj_dd.vector)
         uj_dd.x.scatter_forward()
-        
+
         # Update solution
-        uj.x.array[:] = uj.x.array + dt * uj_d.x.array + 0.5 * dt**2 * uj_dd.x.array
-        uj_d.x.array[:] = uj_d.x.array + dt * uj_dd.x.array
+        uj1_d_array = uj_d.x.array + dt * uj_dd.x.array
+
+        uj.x.array[:] = uj.x.array + 0.5 * dt * (uj_d.x.array + uj1_d_array)
+        uj_d.x.array[:] = uj1_d_array
 
         # Save results into the files
         if i % save_step == 0:
-            p.x.array[:] = ro * c**2 * uj_d.x.array
+            p = project(ro * c**2 * div(uj), domain, ("CG", 1))
 
             xdmf_u.write_function(uj, t)
-            xdmf_u_d.write_function(uj_d, t)
+            xdmf_ud.write_function(uj_d, t)
             xdmf_p.write_function(p, t)
-
+        
         if not has_norm and t >= ct:
             L2_n = L2_norm(uj)
             H1_n = H1_norm(uj)
 
             has_norm = True
-
-    xdmf_u.close()
-    xdmf_u_d.close()
-    xdmf_p.close()
         
+    xdmf_u.close()
+    xdmf_ud.close()
+    xdmf_p.close()
+    
     return L2_n, H1_n
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--n', type=int, help="Number of finite elements",
-                        metavar='N', default=500)
+                        metavar='N', default=50)
     parser.add_argument('--dt', type=float, help="Time integration step",
                         metavar='dt', default=5.0e-8)
     parser.add_argument('--t', type=float, help="Pressure stop time",
@@ -211,8 +211,8 @@ def main():
 
     config = problem_setup(
         N=args.n,
-        points=[0.0, 0.02],
-        p=1.0,
+        points=[[0.0, 0.01], [0.0, 0.01]],
+        p=(-1.0, 0.0, 0.0),
         T=args.T,
         t=args.t,
         ct=args.ct,
--- Non-Stationary-Oscillations/mpi/run.py
+++ Non-Stationary-Oscillations/mpi/run.py
@@ -1,48 +1,163 @@
 #!/usr/bin/env python3
 
-from distutils.cmd import Command
 import subprocess
 
 
-GRID_CONFIG_1 = {
-    'N': [100, 200, 400, 800, 1600, 3200],#, 6400, 12800],
+GRID_CONFIG_0 = {
+    'script': './2d.py',
+    'N': [40],
     'T': 5.0e-5,
     't': 1.5e-6,
     'ct': 2.0e-5,
     'dt': 6.5e-8,
     'fluid': 'Water',
-    'solver': ['PREONLY', 'RICHARDSON', 'CG', 'GMRES', 'TFQMR', 'CR'],
-    'pc': ['ASM', 'BJACOBI', 'GASM', 'GAMG'],
-    'np': [1, 2, 4]
+    'solver': ['preonly', 'gmres', 'gcr'],
+    'pc': ['cholesky', 'lu', 'bjacobi', 'asm'],
+    'np': [1, 2],
+    'skip_params': [
+        ('gmres', 'bjacobi'),
+        ('gmres', 'asm'),
+        ('gcr', 'bjacobi'),
+        ('gcr', 'asm')
+    ]
 }
 
-GRID_CONFIG_2 = {
-    'N': [800],
+# GRID_CONFIG_0 = {
+#     'script': './1d.py',
+#     'N': [51200],
+#     'T': 5.0e-5,
+#     't': 1.5e-6,
+#     'ct': 2.0e-5,
+#     'dt': 6.5e-8,
+#     'fluid': 'Water',
+#     'solver': ['preonly', 'gmres', 'gcr'],
+#     'pc': ['cholesky', 'lu', 'bjacobi', 'asm'],
+#     'np': [1, 2],
+#     'skip_params': []
+# }
+
+GRID_CONFIG_1 = {
+    'script': './1d.py',
+    'N': [100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600, 51200],
     'T': 5.0e-5,
     't': 1.5e-6,
     'ct': 2.0e-5,
-    'dt': 6.5e-9,
+    'dt': 6.5e-8,
     'fluid': 'Water',
-    'solver': ['CG',],
-    'pc': ['ASM', 'BJACOBI', 'GASM'],
-    'np': [1, 2, 4]
+    'solver': ['preonly', 'gmres', 'gcr'],
+    'pc': ['cholesky', 'lu', 'bjacobi', 'asm'],
+    'np': [1, 2, 4, 8, 16],
+    'skip_params': []
+}
+
+GRID_CONFIG_2 = {
+    'script': './2d.py',
+    'N': [20, 40, 80],
+    'T': 5.0e-5,
+    't': 1.5e-6,
+    'ct': 2.0e-5,
+    'dt': 6.5e-7,
+    'fluid': 'Water',
+    'solver': ['preonly', 'gmres', 'gcr'],
+    'pc': ['cholesky', 'lu', 'bjacobi', 'asm'],
+    'np': [1, 2, 4, 8, 16],
+    'skip_params': [
+        ('gmres', 'bjacobi'),
+        ('gmres', 'asm'),
+        ('gcr', 'bjacobi'),
+        ('gcr', 'asm')
+    ]
+}
+
+# TIME_CONFIG_0 = {
+#     'script': './1d.py',
+#     'N': 800,
+#     'T': 5.0e-5,
+#     't': 1.5e-6,
+#     'ct': 2.0e-5,
+#     'dt': [2.0e-7 / 2**i for i in range(5)],
+#     'fluid': 'Water',
+#     'solver': ['preonly', 'gmres', 'gcr'],
+#     'pc': ['cholesky', 'lu', 'bjacobi', 'asm'],
+#     'np': [1, 2],
+#     'skip_params': [
+#         # ('gmres', 'bjacobi'),
+#         # ('gmres', 'asm'),
+#         # ('gcr', 'bjacobi'),
+#         # ('gcr', 'asm')
+#     ]
+# }
+
+TIME_CONFIG_0 = {
+    'script': './2d.py',
+    'N': 20,
+    'T': 5.0e-5,
+    't': 1.5e-6,
+    'ct': 2.0e-5,
+    'dt': [2.0e-7 / 2**i for i in range(5)],
+    'fluid': 'Water',
+    'solver': ['preonly', 'gmres', 'gcr'],
+    'pc': ['cholesky', 'lu', 'bjacobi', 'asm'],
+    'np': [1, 2],
+    'skip_params': [
+        ('gmres', 'bjacobi'),
+        ('gmres', 'asm'),
+        ('gcr', 'bjacobi'),
+        ('gcr', 'asm')
+    ]
+}
+
+TIME_CONFIG_1 = {
+    'script': './1d.py',
+    'N': 250,
+    'T': 5.0e-5,
+    't': 1.5e-6,
+    'ct': 2.0e-5,
+    'dt': [5.0e-7 / 2**i for i in range(8)],
+    'fluid': 'Water',
+    'solver': ['preonly', 'gmres', 'gcr'],
+    'pc': ['cholesky', 'lu', 'bjacobi', 'asm'],
+    'np': [1, 2, 4, 8, 16],
+    'skip_params': []
+}
+
+TIME_CONFIG_2 = {
+    'script': './2d.py',
+    'N': 20,
+    'T': 5.0e-5,
+    't': 1.5e-6,
+    'ct': 2.0e-5,
+    'dt': [5.0e-7 / 2**i for i in range(6)],
+    'fluid': 'Water',
+    'solver': ['preonly', 'gmres', 'gcr'],
+    'pc': ['cholesky', 'lu', 'bjacobi', 'asm'],
+    'np': [1, 2, 4, 8, 16],
+    'skip_params': [
+        ('gmres', 'bjacobi'),
+        ('gmres', 'asm'),
+        ('gcr', 'bjacobi'),
+        ('gcr', 'asm')
+    ]
 }
 
 def compute_grid_convegence(GRID_CONFIG, filename='grig_conv.csv'):
     with open(filename, 'w') as file:
-        file.writelines([';'.join(['solver', 'pc', 'N', 'np', 'L2', 'H1', 'time']), '\n'])
+        file.write('{}\n'.format(';'.join(['solver', 'pc', 'N', 'np', 'L2', 'H1', 'time'])))
 
         for solver in GRID_CONFIG['solver']:
             for pc in GRID_CONFIG['pc']:
+                if (solver, pc) in GRID_CONFIG['skip_params']:
+                    continue
+
                 for N in GRID_CONFIG['N']:
                     for np in GRID_CONFIG['np']:
                         command = [
                             '/usr/bin/time', '-f', '%e', 
-                            'mpirun', '-np', f'{np}', './1d.py',
+                            'mpirun', '-np', f'{np}', GRID_CONFIG["script"],
                             '--n', f'{N}', '--T', f'{GRID_CONFIG["T"]}',
                             '--t', f'{GRID_CONFIG["t"]}', '--ct', f'{GRID_CONFIG["ct"]}',
                             '--dt', f'{GRID_CONFIG["dt"]}', '--fluid', GRID_CONFIG['fluid'],
-                            '--solver', solver, '--pc', pc
+                            '--solver', solver, '--pc', pc, '--no-prefix'
                         ]
                         print(f'Solver: {solver}, PC: {pc}, N: {N}, np: {np}')
 
@@ -54,12 +169,47 @@ def compute_grid_convegence(GRID_CONFIG, filename='grig_conv.csv'):
                         L2 = str(p.stdout.split('\n')[0].split(' = ')[1])
                         H1 = str(p.stdout.split('\n')[1].split(' = ')[1])
                         time = str(p.stderr[:-1])
+                    
+                        file.write('{}\n'.format(';'.join([solver, pc, str(N), str(np), L2, H1, time])))
 
-                        file.writelines([';'.join([solver, pc, str(N), str(np), L2, H1, time]), '\n'])
+def compute_time_convegence(GRID_CONFIG, filename='time_conv.csv'):
+    with open(filename, 'w') as file:
+        file.write('{}\n'.format(';'.join(['solver', 'pc', 'dt', 'np', 'L2', 'H1', 'time'])))
+
+        for solver in GRID_CONFIG['solver']:
+            for pc in GRID_CONFIG['pc']:
+                if (solver, pc) in GRID_CONFIG['skip_params']:
+                    continue
+
+                for dt in GRID_CONFIG['dt']:
+                    for np in GRID_CONFIG['np']:
+                        command = [
+                            '/usr/bin/time', '-f', '%e', 
+                            'mpirun', '-np', f'{np}', GRID_CONFIG["script"],
+                            '--n', f'{GRID_CONFIG["N"]}', '--T', f'{GRID_CONFIG["T"]}',
+                            '--t', f'{GRID_CONFIG["t"]}', '--ct', f'{GRID_CONFIG["ct"]}',
+                            '--dt', f'{dt}', '--fluid', GRID_CONFIG['fluid'],
+                            '--solver', solver, '--pc', pc, '--no-prefix'
+                        ]
+                        print(f'Solver: {solver}, PC: {pc}, dt: {dt}, np: {np}')
+
+                        p = subprocess.run(command, capture_output=True, text=True)
+                        
+                        print(p.stdout, end='')
+                        print(f'Time(s) = {p.stderr}')
+
+                        L2 = str(p.stdout.split('\n')[0].split(' = ')[1])
+                        H1 = str(p.stdout.split('\n')[1].split(' = ')[1])
+                        time = str(p.stderr[:-1])
+                    
+                        file.write('{}\n'.format(';'.join([solver, pc, str(dt), str(np), L2, H1, time])))
 
 
 def main():
-    compute_grid_convegence(GRID_CONFIG_2)
+    compute_grid_convegence(GRID_CONFIG_1, './results/grid_conv_1d.csv')
+    compute_grid_convegence(GRID_CONFIG_2, './results/grid_conv_2d.csv')
+    compute_time_convegence(TIME_CONFIG_1, './results/time_conv_1d.csv')
+    compute_time_convegence(TIME_CONFIG_2, './results/time_conv_2d.csv')
 
 
 if __name__ == "__main__":
